9478774d41d019f8b9bd743cd80753c5266724ad
robertjchandler <robertjchandler4@gmail.com>
2025-03-04 17:30:44 -0600

    add Line

--- window.py
+++ window.py
@@ -2,9 +2,9 @@ from tkinter import Tk, BOTH, Canvas
 
 class Window():
     def __init__(self, width, height):
-        self.__root = Tk()
+        self.__root = Tk()        
         self.__root.title("Maze Solver")
-        self.__canvas = Canvas(self.__root)
+        self.__canvas = Canvas(self.__root, height=height, width=width)
         self.__canvas.pack()
         self.running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)        
@@ -20,3 +20,7 @@ class Window():
 
     def close(self):
         self.running = False
+
+    def draw_line(self, line, fill_color):
+        line.draw(self.__canvas, fill_color)
+        
