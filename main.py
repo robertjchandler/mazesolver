9d05095fce354ec41119916234b351dba0cc0e11
robertjchandler <robertjchandler4@gmail.com>
2025-03-05 16:58:10 -0600

    update Line

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from window import *
+from window import Window
+from line import Line
+from point import Point
 
 def main():
     win = Window(800, 600)
+    l = Line(Point(50, 50), Point(400, 400))
+    win.draw_line(l, "black")
     win.wait_for_close()
     
+main()
